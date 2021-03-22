3b3cc5a40c9fdc5697a5a5013532915ee14da369
jorgesanme <jorgesanme@gmail.com>
2021-03-22 13:43:23 +0000

    la celda Welcome aparece pero sin redondear

--- DiscourseClient/Topics/WellcomeViewCell.swift
+++ DiscourseClient/Topics/WellcomeViewCell.swift
@@ -11,40 +11,7 @@ import UIKit
 class WellcomeViewCell: UITableViewCell {
 
    
-    @IBOutlet weak var orangeView: UIView!
-    
-//    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-////        self.orangeView.layer.cornerRadius = 8
-//        super.init(style: style, reuseIdentifier: reuseIdentifier)
-//        
-//    }
-//    
-//    required init?(coder: NSCoder) {
-//        fatalError("init(coder:) has not been implemented")
-//    }
-    
-    
-    
-//    func commonInit(){
-//        orangeView.layer.cornerRadius = 8
-//        let bundle = Bundle(for: WellcomeViewCell.self)
-//        if let bundleViews = bundle.loadNibNamed("WellcomeViewCell", owner: self, options: nil),
-//           let contenView = bundleViews.first as? UIView {
-//            contenView.translatesAutoresizingMaskIntoConstraints = false
-//            addSubview(contenView)
-//            NSLayoutConstraint.activate([
-//                contenView.topAnchor.constraint(equalTo: self.topAnchor),
-//                contenView.bottomAnchor.constraint(equalTo: self.bottomAnchor),
-//                contenView.leftAnchor.constraint(equalTo: self.leftAnchor),
-//                contenView.rightAnchor.constraint(equalTo: self.rightAnchor)
-//            ])
-//        }
-//        layer.borderColor = UIColor.black.cgColor
-//        layer.borderWidth = 5
-//        
-//    }
-//    
-    
-    
+  
+   
     
 }
